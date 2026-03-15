c5f666c03a54198ae493b51ca61d96b5936715c1
Murilo Damario <murilo.gcdamario@gmail.com>
2026-03-15 18:11:35 -0300

    :lipstick: feat: Apply padding on input

--- src/components/AppInput/index.tsx
+++ src/components/AppInput/index.tsx
@@ -53,25 +53,28 @@ export const AppInput = <T extends FieldValues>({
           <View className='w-full mt-4'>
             {label && (
               <Text
-                className={clsx('mb-2 mt-3 text-base', isFocused ? 'text-accent-brand' : 'text-gray-600')}
+                className={clsx(
+                  'mb-2 mt-3 text-base',
+                  isFocused ? 'text-accent-brand' : 'text-gray-600',
+                )}
               >
                 {label}
               </Text>
             )}
 
-            <TouchableOpacity className='flex-row items-center justify-between border-b-[1px] border-gray-600 py-2 h-16'>
+            <TouchableOpacity className='flex-row items-center justify-between border-b border-gray-600 px-3 py-2 h-16'>
               {leftIconName && (
                 <MaterialIcons
                   name={leftIconName}
-                  color={isFocused ? colors['accent-brand'] : colors.gray['600']}
                   size={24}
+                  color={isFocused ? colors['accent-brand'] : colors.gray[600]}
                   className='mr-2'
                 />
               )}
               <TextInput
                 value={value}
                 onChangeText={onChange}
-                placeholderTextColor={colors.gray['700']}
+                placeholderTextColor={colors.gray[700]}
                 className='flex-1 text-base text-gray-500'
                 onFocus={checkFocus}
                 onEndEditing={checkFocus}
@@ -80,17 +83,17 @@ export const AppInput = <T extends FieldValues>({
                 {...rest}
               />
 
-              {
-                secureTextEntry && (
-                  <TouchableOpacity onPress={() => setShowText((value) => !value)}>
-                    <MaterialIcons
-                      name={showText ? 'visibility' : 'visibility-off'}
-                      color={colors.gray['600']}
-                      size={24}
-                    />
-                  </TouchableOpacity>
-                )
-              }
+              {secureTextEntry && (
+                <TouchableOpacity
+                  onPress={() => setShowText((value) => !value)}
+                >
+                  <MaterialIcons
+                    name={showText ? 'visibility' : 'visibility-off'}
+                    color={colors.gray[600]}
+                    size={24}
+                  />
+                </TouchableOpacity>
+              )}
             </TouchableOpacity>
           </View>
         )
@@ -98,4 +101,3 @@ export const AppInput = <T extends FieldValues>({
     />
   )
 }
-
