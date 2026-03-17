aee4cc2600f252b03732ff128db107faaadfda3b
Murilo Damario <murilo.gcdamario@gmail.com>
2026-03-16 23:58:25 -0300

    :sparkles: feat: Create Snackbar context

--- App.tsx
+++ App.tsx
@@ -1,14 +1,17 @@
 import NavigationRoutes from '@/routes'
 
 import { AuthContextProvivider } from '@/context/auth.context'
+import { SnackbarContextProvider } from '@/context/snackbar.context'
 
 import './src/styles/global.css'
 
 export default function App() {
   return (
-    <AuthContextProvivider>
-      <NavigationRoutes />
-    </AuthContextProvivider>
+    <SnackbarContextProvider>
+      <AuthContextProvivider>
+        <NavigationRoutes />
+      </AuthContextProvivider>
+    </SnackbarContextProvider>
   )
 }
 
