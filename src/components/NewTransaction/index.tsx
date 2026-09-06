e23eb70b3c2fdc2854c65d5dd6a2b67e94ffe4f2
Murilo Damario <murilo.gcdamario@gmail.com>
2026-09-06 09:20:44 -0300

    :sparkles: feat: Add currency input field to NewTransaction component

--- src/components/NewTransaction/index.tsx
+++ src/components/NewTransaction/index.tsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import { MaterialIcons } from '@expo/vector-icons'
-import { Text, TouchableOpacity, View } from 'react-native'
+import { Text, TextInput, TouchableOpacity, View } from 'react-native'
+import CurrencyInput from 'react-native-currency-input'
 
 import { useBottomSheetContext } from '@/context/bottomSheet.context'
 
@@ -17,6 +18,10 @@ export const NewTransaction = () => {
     value: 0
   })
 
+  const setTransactionData = (key: keyof CreateTransactionRequest, value: string | number) => {
+    setTransaction((prevData) => ({ ...prevData, [key]: value }))
+  }
+
   return (
     <View className='px-8 py5'>
       <TouchableOpacity className='w-full flex-row items-center justify-between'>
@@ -30,6 +35,27 @@ export const NewTransaction = () => {
           onPress={closeBottomSheet}
         />
       </TouchableOpacity>
+
+      <View className='flex-1 mt-8 mb-8'>
+        <TextInput
+          className='text-white text-lg h-[50] bg-background-primary my-2 rounded-[6] pl-4'
+          placeholder='Descrição'
+          placeholderTextColor={colors.gray[700]}
+          value={transaction.description}
+          onChangeText={(text) => setTransactionData('description', text)}
+        />
+
+        <CurrencyInput
+          className='text-white text-lg h-[50] bg-background-primary my-2 rounded-[6] pl-4'
+          value={transaction.value}
+          prefix='R$'
+          delimiter='.'
+          separator=','
+          precision={2}
+          minValue={0}
+          onChangeValue={(value) => setTransactionData('value', value ?? 0)}
+        />
+      </View>
     </View>
   )
 }
